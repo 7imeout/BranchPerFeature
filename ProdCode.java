0cf7e0bb06078be19d5521fbbabd27f60ba1d3a0
Mike Dongyub Ryu <doryu@calpoly.edu>
2016-02-05 12:36:31 -0800

    HelperInProdCode : ProdCode's main now uses a helper method instead

--- ProdCode.java
+++ ProdCode.java
@@ -1,5 +1,9 @@
 public class ProdCode {
     public static void main(String[] args) {
+        mainTask();
+    }
+
+    public static void mainTask() {
         System.out.println("This is Good Prod Code");
     }
 }
